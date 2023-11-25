1925911735b8a12645af9da62894db1cda1e4355
Tobias Hintzsche <tobias.hintzsche@dertouristik.com>
2023-11-25 17:29:22 +0100

    style datepicker

--- components/HomePage/DatePickerTest.tsx
+++ components/HomePage/DatePickerTest.tsx
@@ -21,21 +21,88 @@ const StyledDateRangePicker = styled(DateRangePicker)`
   /* Add more styles as needed */
 `;
 
+const { beforeToday } = DateRangePicker;
+
 export const DatePickerTest = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const newDate = new Date();
+  const currentYear = newDate.getFullYear();
+
+  // Calculate the year 3 years from now
+  const threeYearsFromNow = currentYear + 3;
+
+  const [value, setValue] = useState<DateRange | null>(null);
 
   const dateRange: DateRange = [newDate, newDate];
+
+  // Function to check if a date is in the past
+  const isDateInPast = (date: Date) => {
+    const currentDate = new Date();
+    // Extract year, month, and day components for comparison
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
+
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+
+    // Compare year, month, and day
+    return (
+      year < currentYear ||
+      (year === currentYear &&
+        (month < currentMonth || (month === currentMonth && day < currentDay)))
+    );
+  };
 
   return (
     <div className="z-50">
       <StyledDateRangePicker
         isoWeek
-        appearance="subtle"
+        limitStartYear={1}
+        limitEndYear={3}
+        character=" - "
+        locale={{
+          sunday: "So",
+          monday: "Mo",
+          tuesday: "Di",
+          wednesday: "Mi",
+          thursday: "Do",
+          friday: "Fr",
+          saturday: "Sa",
+          today: "Heute",
+          yesterday: "Gestern",
+          ok: "Übernehmen",
+        }}
+        placement="bottom"
+        shouldDisableDate={beforeToday && beforeToday()}
+        appearance="default"
         placeholder="Reisezeitraum wählen"
-        value={dateRange}
+        value={value}
+        format="dd.MM.yyyy"
+        onChange={setValue}
+        showMeridian={false}
+        renderExtraFooter={() => (
+          <div
+            style={{
+              backgroundColor: "green",
+              padding: 10,
+              color: "white",
+            }}
+          >
+            <h4>Extra Footer</h4>
+          </div>
+        )}
       />
+    </div>
+  );
+};
+
+export const ExtraFooter = () => {
+  return (
+    <div className="w-full h-20 bg-black">
+      <div>test</div>
     </div>
   );
 };
